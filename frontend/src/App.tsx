714b9d549d0df20c5f50d833274c4488c0fcf4bb
ivan.dario <ivan.dario@nexiona.com>
2021-06-24 13:48:26 +0200

    added basic files and layout for public routes with login and sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import "./App.css";
+
+//pages
+import Login from "PublicViews/Login/";
+import SignUp from "PublicViews/SignUp";
+
+const PrivateRoute = () => {
+  const authenticated = false;
+  if(authenticated === false){
+    return <Redirect to="/login" />
+  } else
+    return <Route path="/" component={() => <>Home</>}></Route>
+}
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/sign-up" component={SignUp}></Route>
+          <PrivateRoute />
+        </Switch>
+      </Router>
     </div>
   );
 }
