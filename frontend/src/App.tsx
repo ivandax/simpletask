5a6e3091e61ce5223bc787bb3adcaf91ad9ec80e
ivan.dario <ivan.dario@nexiona.com>
2021-06-24 14:00:13 +0200

    installed and applied prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,25 +7,24 @@ import Login from "PublicViews/Login/";
 import SignUp from "PublicViews/SignUp";
 
 const PrivateRoute = () => {
-  const authenticated = false;
-  if(authenticated === false){
-    return <Redirect to="/login" />
-  } else
-    return <Route path="/" component={() => <>Home</>}></Route>
-}
+    const authenticated = false;
+    if (authenticated === false) {
+        return <Redirect to="/login" />;
+    } else return <Route path="/" component={() => <>Home</>}></Route>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/sign-up" component={SignUp}></Route>
-          <PrivateRoute />
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/sign-up" component={SignUp}></Route>
+                    <PrivateRoute />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
